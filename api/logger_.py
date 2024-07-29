1b7bc049f4c9bfebd03a91b90efb94df238efca3
molanp <104612722+molanp@users.noreply.github.com>
2024-07-29 12:27:46 +0800

    x

--- api/logger_.py
+++ api/logger_.py
@@ -3,16 +3,19 @@ from datetime import datetime
 import threading
 import os
 
+
 class Logger_:
     def __init__(self, log_file_prefix=os.path.join(os.path.dirname(__file__), 'logs')):
         os.makedirs(log_file_prefix, exist_ok=True)
-        self.log_file = os.path.join(log_file_prefix,f"{datetime.now().strftime('%Y-%m-%d')}.log")
+        self.log_file = os.path.join(
+            log_file_prefix, f"{datetime.now().strftime('%Y-%m-%d')}.log")
         self.logger = logging.getLogger("Custom Logger")
         self.logger.setLevel(logging.INFO)
         # 创建一个控制台输出的handler，并设置级别为WARN
         console_handler = logging.StreamHandler()
         console_handler.setLevel(logging.WARN)
-        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+        formatter = logging.Formatter(
+            '%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
         console_handler.setFormatter(formatter)
         self.logger.addHandler(console_handler)
 
@@ -24,6 +27,10 @@ class Logger_:
 
         # 添加线程锁
         self.lock = threading.Lock()
+
+    def debug(self, message):
+        with self.lock:
+            self.logger.debug(message)
 
     def info(self, message):
         with self.lock:
