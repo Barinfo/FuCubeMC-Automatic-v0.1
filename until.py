c119b6c53642d7ca4413f249c286aef25142b241
molanp <104612722+molanp@users.noreply.github.com>
2024-07-30 00:29:04 +0800

    uuid

--- until.py
+++ until.py
@@ -248,7 +248,7 @@ class Mcsm:
             response = requests.get(api_url, headers=headers)
             if response.status_code == 200:
                 self.logger.debug(f"User found uuid successfully. Response: {response.json()}")
-                return response.json()["data"][0]["uuid"]
+                return response.json()["data"]["data"][0]["uuid"]
             else:
                 self.logger.error(
                     f"Failed to find uuid. Status code: {response.status_code}\nMessage: {response.json()['data']}")
