c84e11c1a98980a58e920490eb3f1cfc34662f62
Yeying-Xingchen <yeyingxingchen@yeah.net>
2024-07-30 12:37:25 +0800

    新增 实例页面。

--- panel/__init__.py
+++ panel/__init__.py
@@ -5,5 +5,9 @@ app = Blueprint('panel', __name__)
 
 @app.route('/')
 def index():
+    return render_template("panel/index.html")
+
+@app.route('/instance/')
+def instance():
     panel_addr = config["mcsm"]["url"]
-    return render_template("panel/index.html", panel_addr=panel_addr)
+    return render_template("panel/instance.html", panel_addr=panel_addr)
