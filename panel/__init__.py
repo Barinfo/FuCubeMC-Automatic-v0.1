f40d90402ddd496387d879da57ec1db4bd9382b6
Yeying-Xingchen <yeyingxingchen@yeah.net>
2024-07-30 12:34:11 +0800

    修复 面板地址错误。

--- panel/__init__.py
+++ panel/__init__.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
+from config import config
 
 app = Blueprint('panel', __name__)
 
 @app.route('/')
 def index():
-    return render_template("panel/index.html")
+    panel_addr = config["mcsm"]["url"]
+    return render_template("panel/index.html", panel_addr=panel_addr)
