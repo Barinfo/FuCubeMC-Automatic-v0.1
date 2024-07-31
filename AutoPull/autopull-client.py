873204a70d2daf10aa2f961b6de012c447a145c5
molanp <104612722+molanp@users.noreply.github.com>
2024-07-31 16:48:23 +0800

    在客户端的同步功能中实现进度条显示
    此次更新在同步命令发送功能中集成了进度条显示。通过`select`模块监控socket的可读性，从而在同步进行中以假定的最长时间为基准，逐步填充进度条，为用户提供同步过程的视觉反馈。当接收到来自服务器的响应时，进度条将被填满，并显示“同步完成”的状态。此外，窗口尺寸进行了适当调整，以适应新增的进度条组件。

--- AutoPull/autopull-client.py
+++ AutoPull/autopull-client.py
@@ -1,11 +1,13 @@
 import socket
+import select
 import tkinter as tk
 from tkinter import messagebox
+from tkinter import ttk
 
-def send_command():
+def send_command(progress_var, button, status_label):
     host = 's1.ultrasnd.cn'
     port = 7481
-    command = 'C:\\Users\\Administrator\\Desktop\\pull.bat'
+    command = 'C:/Users/Administrator/Desktop/pull.bat'
 
     try:
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,22 +15,53 @@ def send_command():
 
         client_socket.sendall(command.encode())
 
-        output = client_socket.recv(4096).decode()
-        print(f"Command output:\n{output}")
+        status_label.config(text="同步中...")
+        button.config(state=tk.DISABLED)
+
+        for i in range(81):
+            root.update_idletasks()
+            progress_var.set(i)
+            root.update()
+
+            ready_to_read, _, _ = select.select([client_socket], [], [], 0.1)
+            if ready_to_read:
+                output = client_socket.recv(4096).decode()
+                break
+
+            root.after(max(10, i * 5))
+
+        if not output:
+            output = client_socket.recv(4096).decode()
+
+        #print(f"Command output:\n{output}")
 
         client_socket.close()
 
-        messagebox.showinfo("Success", "已完成同步")
+        progress_var.set(100)
+        button.config(state=tk.NORMAL)
+
+        status_label.config(text="同步完成")
+
+        messagebox.showinfo("Success", f"已完成同步")
     except Exception as e:
         messagebox.showerror("Error", f"同步失败: {e}")
 
 # 创建主窗口
 root = tk.Tk()
 root.title("同步工具")
-root.geometry("250x100")
+root.geometry("400x180")  # 调整窗口大小以适应更宽的进度条
+
+# 创建进度条
+progress_var = tk.IntVar()
+progress_bar = ttk.Progressbar(root, variable=progress_var, maximum=100)
+progress_bar.pack(fill=tk.X, expand=True)  # 修改这里让进度条充满窗口宽度
+
+# 创建状态标签
+status_label = tk.Label(root, text="", font=("Helvetica", 10))
+status_label.pack()
 
 # 创建一个按钮
-sync_button = tk.Button(root, text="同步", command=send_command ,width=20, height=2)
+sync_button = tk.Button(root, text="同步", command=lambda: send_command(progress_var, sync_button, status_label), width=20, height=2)
 sync_button.pack(pady=20)
 
 # 运行主循环
