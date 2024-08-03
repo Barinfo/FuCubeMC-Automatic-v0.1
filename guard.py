8bc1d681aec538100fd2c0c7c8a4ff50e0796037
bilibilijandy <141329748+bilibilijandy@users.noreply.github.com>
2024-08-03 19:17:54 +0800

    更新 guard.py

--- guard.py
+++ guard.py
@@ -1,16 +1,20 @@
 import requests
+import time
 
 
 timeout=1
+while True:
+    Time=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+    try:
+        response=requests.get('http://s1.rancloud.xyz:24444',timeout=timeout)
+        print(Time+"守护进程正常")
+    except:
+        print(Time+"守护进程下线!")
 
-try:
-    response=requests.get('http://s1.rancloud.xyz:24444',timeout=timeout)
-    print(response.text)
-except:
-    print("守护进程下线!")
+    try:
+        response=requests.get('http://s1.rancloud.xyz:23333',timeout=timeout)
+        print(Time+"网页进程正常")
+    except:
+        print(Time+"网页进程下线!")
 
-try:
-    response=requests.get('http://s1.rancloud.xyz:23333',timeout=timeout)
-    print(response.text)
-except:
-    print("网页进程下线!")
+    time.sleep(10)
